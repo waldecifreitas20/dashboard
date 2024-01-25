72425bec5fbe7b8be7ff6f7ebc77689de57bb159
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-01-25 11:33:02 -0300

    feat: aside footer privacity toggler

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const btnMenu = document.querySelector("#btn-menu");
 const menu = document.getElementById("aside");
 
-
+/* active menu toggler */
 btnMenu.addEventListener('change', (evt)=> {
     let isChecked = evt.target.checked;
     
@@ -12,4 +12,15 @@ btnMenu.addEventListener('change', (evt)=> {
         menu.classList.remove('dispose');
         menu.classList.add('show');
     }
+});
+
+const radioGroup = document.querySelectorAll("#aside-footer .radio-group");
+
+const btnPublicPrivacy = document.getElementById("public-radio").addEventListener('click', () => {
+    radioGroup[0].classList.add('active');
+    radioGroup[1].classList.remove('active');
+});
+const btnPrivatePrivacy = document.getElementById("private-radio").addEventListener('click', () => {
+    radioGroup[0].classList.remove('active');
+    radioGroup[1].classList.add('active');
 });
